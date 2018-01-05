1596b4e20231bb10e73947dc9593ec2c3b552c30
Tomohiro Kumagai <tomohiro@ez-style.jp>
2018-01-05 14:09:54 +0900

    cdn.rawgit.com に対応しました。

--- Setup/Safari.sh
+++ Setup/Safari.sh
@@ -2,8 +2,9 @@
 
 BOOKMARK_PLIST="${HOME}/Library/Safari/Bookmarks.plist"
 BOOKMARKLET_TITLE="東横INN の予約をカレンダーに登録"
-BOOKMARKLET_URL="javascript:%28function%28%29%20%7B%0A%09const%20e%20%3D%20document.createElement%28%27script%27%29%3B%0A%09const%20url%20%3D%20%27https%3A//rawgit.com/es-kumagai/Bookmarklet.ToyokoInnToCalendar/%%BRANCH%%/ToyokoInnToCalendar.js%27%3B%0A%09e.charset%3D%27utf-8%27%3B%0A%09e.src%3Durl%3B%0A%09document.body.appendChild%28e%29%3B%0A%7D%29%28%29"
+BOOKMARKLET_URL="javascript:%28function%28%29%20%7B%0A%09const%20e%20%3D%20document.createElement%28%27script%27%29%3B%0A%09const%20url%20%3D%20%27%%BOOKMARKLET_SCRIPT_URL%%%27%3B%0A%09e.charset%3D%27utf-8%27%3B%0A%09e.src%3Durl%3B%0A%09document.body.appendChild%28e%29%3B%0A%7D%29%28%29"
 FOLDER_TITLE="Bookmarklet"
+BOOKMARKLET_SCRIPT_URL="https://rawgit.com/es-kumagai/Bookmarklet.ToyokoInnToCalendar/%%BRANCH%%/ToyokoInnToCalendar.js"
 
 DATE=$(date +%Y%m%d-%H%M%S)
 FOLDER_UUID=$(uuidgen)
@@ -42,11 +43,16 @@ if [ "$1" != "" ]
 then
 	BRANCH="$1"
 	TITLE_SUFFIX=" (${BRANCH})"
+	URL_PREFIX=""
 else
 	BRANCH="master"
 	TITLE_SUFFIX=""
+	URL_PREFIX="cdn."
 fi
 
+BOOKMARKLET_SCRIPT_URL=$(echo "${BOOKMARKLET_SCRIPT_URL}" | sed -e "s#/#\\\\/#g")
+BOOKMARKLET_URL=$(echo "${BOOKMARKLET_URL}" | sed -e "s/%%BOOKMARKLET_SCRIPT_URL%%/${BOOKMARKLET_SCRIPT_URL}/")
+BOOKMARKLET_URL=$(echo "${BOOKMARKLET_URL}" | sed -e "s#://#%3A//${URL_PREFIX}#")
 BOOKMARKLET_URL=$(echo "${BOOKMARKLET_URL}" | sed -e "s/%%BRANCH%%/${BRANCH}/")
 
 # Replace '/' with '//'.
